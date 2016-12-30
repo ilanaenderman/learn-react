771407b202c050dd298f0849ad6c1bdc767b4807
Ilana <ilanaenderman@hotmail.com>
2016-12-30 10:55:03 +0100

    added place where to find tutorial

--- App.jsx
+++ App.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 
 class App extends React.Component {
+   constructor() {
+      super();
+		
+      this.state = {
+         data: 
+         [
+            {
+               "id":1,
+               "name":"Foo",
+               "age":"20"
+            },
+				
+            {
+               "id":2,
+               "name":"Bar",
+               "age":"30"
+            },
+				
+            {
+               "id":3,
+               "name":"Baz",
+               "age":"40"
+            }
+         ]
+      }
+   }
+	
    render() {
       return (
          <div>
             <Header/>
             <Content/>
+            <table>
+               <tbody>
+                  {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
+               </tbody>
+            </table>
          </div>
       );
    }
@@ -25,11 +57,23 @@ class Content extends React.Component {
    render() {
       return (
          <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
+            <p>This is the content</p>
          </div>
       );
    }
 }
 
-export default Header;
+class TableRow extends React.Component {
+   render() {
+      return (
+         <tr>
+            <td>{this.props.data.id}</td>
+            <td>{this.props.data.name}</td>
+            <td>{this.props.data.age}</td>
+         </tr>
+      );
+   }
+}
+
+export default App;
+
--- main.js
+++ main.js
@@ -3,3 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// https://www.tutorialspoint.com/reactjs/reactjs_components.htm
+// Tutorial to learn react.JS
